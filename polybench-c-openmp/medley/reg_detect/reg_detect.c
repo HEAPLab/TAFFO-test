1df3c3b204b79d3af263bf8e242ad71a6f42e728
klaa97 <10521251@polimi.it>
2021-01-07 20:16:29 +0100

    Add annotations to remaining benchmarks

--- polybench-c-openmp/medley/reg_detect/reg_detect.c
+++ polybench-c-openmp/medley/reg_detect/reg_detect.c
@@ -27,7 +27,8 @@ void init_array(int maxgrid,
 		DATA_TYPE POLYBENCH_2D(mean,MAXGRID,MAXGRID,maxgrid,maxgrid),
 		DATA_TYPE POLYBENCH_2D(path,MAXGRID,MAXGRID,maxgrid,maxgrid))
 {
-  int i, j;
+  int i __attribute__((annotate("scalar(range(0, " PB_XSTR(MAXGRID) ") final)")));
+  int j __attribute__((annotate("scalar(range(0, " PB_XSTR(MAXGRID) ") final)")));
 
   for (i = 0; i < maxgrid; i++)
     for (j = 0; j < maxgrid; j++) {
@@ -75,12 +76,12 @@ void kernel_reg_detect(int niter, int maxgrid, int length,
     {
       for (t = 0; t < _PB_NITER; t++)
       {
-        #pragma omp for private (i, cnt) collapse(2) schedule(static)
+        #pragma omp parallel for private (i, cnt) collapse(2) schedule(static)
         for (j = 0; j <= _PB_MAXGRID - 1; j++)
           for (i = j; i <= _PB_MAXGRID - 1; i++)
             for (cnt = 0; cnt <= _PB_LENGTH - 1; cnt++)
               diff[j][i][cnt] = sum_tang[j][i];
-        #pragma omp for private (i, cnt) collapse(2) schedule(static)
+        #pragma omp parallel for private (i, cnt) collapse(2) schedule(static)
         for (j = 0; j <= _PB_MAXGRID - 1; j++)
         {
           for (i = j; i <= _PB_MAXGRID - 1; i++)
@@ -91,10 +92,10 @@ void kernel_reg_detect(int niter, int maxgrid, int length,
             mean[j][i] = sum_diff[j][i][_PB_LENGTH - 1];
 	        }
         }
-        #pragma omp for
+        #pragma omp parallel for
         for (i = 0; i <= _PB_MAXGRID - 1; i++)
           path[0][i] = mean[0][i];
-        #pragma omp for private (i) collapse(2) schedule(static)
+        #pragma omp parallel for private (i) collapse(2) schedule(static)
         for (j = 1; j <= _PB_MAXGRID - 1; j++)
           for (i = j; i <= _PB_MAXGRID - 1; i++)
             path[j][i] = path[j - 1][i - 1] + mean[j][i];
@@ -112,11 +113,11 @@ int main(int argc, char** argv)
   int length = LENGTH;
 
   /* Variable declaration/allocation. */
-  POLYBENCH_2D_ARRAY_DECL(sum_tang, DATA_TYPE, MAXGRID, MAXGRID, maxgrid, maxgrid);
-  POLYBENCH_2D_ARRAY_DECL(mean, DATA_TYPE, MAXGRID, MAXGRID, maxgrid, maxgrid);
-  POLYBENCH_2D_ARRAY_DECL(path, DATA_TYPE, MAXGRID, MAXGRID, maxgrid, maxgrid);
-  POLYBENCH_3D_ARRAY_DECL(diff, DATA_TYPE, MAXGRID, MAXGRID, LENGTH, maxgrid, maxgrid, length);
-  POLYBENCH_3D_ARRAY_DECL(sum_diff, DATA_TYPE, MAXGRID, MAXGRID, LENGTH, maxgrid, maxgrid, length);
+  POLYBENCH_2D_ARRAY_DECL(sum_tang, DATA_TYPE __attribute__((annotate("target('sum_tang') scalar()"))), MAXGRID, MAXGRID, maxgrid, maxgrid);
+  POLYBENCH_2D_ARRAY_DECL(mean, DATA_TYPE __attribute__((annotate("target('mean') scalar(range(0, 5000) final)"))), MAXGRID, MAXGRID, maxgrid, maxgrid);
+  POLYBENCH_2D_ARRAY_DECL(path, DATA_TYPE __attribute__((annotate("target('path') scalar(range(0, 5000) final)"))), MAXGRID, MAXGRID, maxgrid, maxgrid);
+  POLYBENCH_3D_ARRAY_DECL(diff, DATA_TYPE __attribute__((annotate("target('diff') scalar(range(0, 5000) final)"))), MAXGRID, MAXGRID, LENGTH, maxgrid, maxgrid, length);
+  POLYBENCH_3D_ARRAY_DECL(sum_diff, DATA_TYPE __attribute__((annotate("target('sum_diff') scalar(range(0, 5000) final)"))), MAXGRID, MAXGRID, LENGTH, maxgrid, maxgrid, length);
   
   /* Initialize array(s). */
   init_array (maxgrid,
--- polybench-c-openmp/stencils/fdtd-2d/fdtd-2d.c
+++ polybench-c-openmp/stencils/fdtd-2d/fdtd-2d.c
@@ -30,7 +30,8 @@ void init_array (int tmax,
 		 DATA_TYPE POLYBENCH_2D(hz,NX,NY,nx,ny),
 		 DATA_TYPE POLYBENCH_1D(_fict_,TMAX,tmax))
 {
-  int i, j;
+  int i __attribute__((annotate("scalar(range(0, " PB_XSTR(NX) ") final)")));
+  int j __attribute__((annotate("scalar(range(0, " PB_XSTR(NY) ") final)")));
 
   for (i = 0; i < tmax; i++)
     _fict_[i] = (DATA_TYPE) i;
@@ -85,25 +86,21 @@ void kernel_fdtd_2d(int tmax,
     {
       for (t = 0; t < _PB_TMAX; t++)
       {
-        #pragma omp for
+        #pragma omp parallel for
         for (j = 0; j < _PB_NY; j++)
           ey[0][j] = _fict_[t];
-        #pragma omp barrier
-        #pragma omp for collapse(2) schedule(static)
+        #pragma omp parallel for collapse(2) schedule(static)
         for (i = 1; i < _PB_NX; i++)
           for (j = 0; j < _PB_NY; j++)
             ey[i][j] = ey[i][j] - 0.5*(hz[i][j]-hz[i-1][j]);
-        #pragma omp barrier
-        #pragma omp for collapse(2) schedule(static)
+        #pragma omp parallel for collapse(2) schedule(static)
         for (i = 0; i < _PB_NX; i++)
           for (j = 1; j < _PB_NY; j++)
             ex[i][j] = ex[i][j] - 0.5*(hz[i][j]-hz[i][j-1]);
-        #pragma omp barrier
-        #pragma omp for collapse(2) schedule(static)
+        #pragma omp parallel for collapse(2) schedule(static)
         for (i = 0; i < _PB_NX - 1; i++)
           for (j = 0; j < _PB_NY - 1; j++)
             hz[i][j] = hz[i][j] - 0.7*  (ex[i][j+1] - ex[i][j] + ey[i+1][j] - ey[i][j]);
-        #pragma omp barrier
       }
     }
   }
@@ -119,10 +116,10 @@ int main(int argc, char** argv)
   int ny = NY;
 
   /* Variable declaration/allocation. */
-  POLYBENCH_2D_ARRAY_DECL(ex,DATA_TYPE,NX,NY,nx,ny);
-  POLYBENCH_2D_ARRAY_DECL(ey,DATA_TYPE,NX,NY,nx,ny);
-  POLYBENCH_2D_ARRAY_DECL(hz,DATA_TYPE,NX,NY,nx,ny);
-  POLYBENCH_1D_ARRAY_DECL(_fict_,DATA_TYPE,TMAX,tmax);
+  POLYBENCH_2D_ARRAY_DECL(ex,DATA_TYPE __attribute__((annotate("target('ex') scalar(range(-1000,1000) final)"))),NX,NY,nx,ny);
+  POLYBENCH_2D_ARRAY_DECL(ey,DATA_TYPE __attribute__((annotate("target('ey') scalar(range(-1000,1000) final)"))),NX,NY,nx,ny);
+  POLYBENCH_2D_ARRAY_DECL(hz,DATA_TYPE __attribute__((annotate("target('hz') scalar(range(-1000,1000) final)"))),NX,NY,nx,ny);
+  POLYBENCH_1D_ARRAY_DECL(_fict_,DATA_TYPE __attribute__((annotate("target('_fict_') scalar()"))),TMAX,tmax);
 
   /* Initialize array(s). */
   init_array (tmax, nx, ny,
