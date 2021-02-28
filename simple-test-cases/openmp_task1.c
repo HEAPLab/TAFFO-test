0ec1ab6874787ad0856a58ed2bbed8c996488c52
klaa97 <10521251@polimi.it>
2021-02-28 23:48:58 +0100

    Fix wrong annotation in test

--- simple-test-cases/openmp_task1.c
+++ simple-test-cases/openmp_task1.c
@@ -1,16 +1,16 @@
-/// TAFFO_TEST_ARGS -fopenmp
+///TAFFO_TEST_ARGS -fopenmp
 #include <stdio.h>
 
 #define N (100)
 
 int main(int argc, char *argv[]) {
-  float foo __attribute((annotate("target('foo') scalar(range(0,10)) final"))) =
+  float foo __attribute((annotate("target('foo') scalar(range(0,10) final)"))) =
       1.0f;
   float pop
-      __attribute((annotate("target('pop') scalar(range(0,2000)) final"))) =
+      __attribute((annotate("target('pop') scalar(range(0,2000) final)"))) =
           363;
   float result
-      __attribute((annotate("target('result') scalar(range(0,2000)) final"))) =
+      __attribute((annotate("target('result') scalar(range(0,2000) final)"))) =
           0;
 
 #pragma omp parallel num_threads(4)
