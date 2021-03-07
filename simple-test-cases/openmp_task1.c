7f9275f5f36813c0a4c1759e2a91a46726a15d10
klaa97 <10521251@polimi.it>
2021-03-07 12:51:06 +0100

    Add new openmp tests and cleanup old tests

--- simple-test-cases/openmp_task1.c
+++ simple-test-cases/openmp_task1.c
@@ -4,27 +4,27 @@
 #define N (100)
 
 int main(int argc, char *argv[]) {
-  float foo __attribute((annotate("target('foo') scalar(range(0,10) final)"))) =
+  float first_task_var __attribute((annotate("target('first_task_var') scalar(range(0,10) final)"))) =
       1.0f;
-  float pop
-      __attribute((annotate("target('pop') scalar(range(0,2000) final)"))) =
+  float second_task_var
+      __attribute((annotate("target('second_task_var') scalar(range(0,2000) final)"))) =
           363;
   float result
       __attribute((annotate("target('result') scalar(range(0,2000) final)"))) =
-          0;
+          0.0;
 
 #pragma omp parallel num_threads(4)
   {
 #pragma omp single
     {
-      foo += 1.0;
+      first_task_var += 1.0;
 #pragma omp task
-      { foo *= 2.1f; }
+      { first_task_var *= 2.1f; }
 #pragma omp task
-      pop *= 5.4f;
+      second_task_var *= 5.4f;
     }
   }
-  result = pop + foo;
+  result = first_task_var + second_task_var;
 
-  printf("result: %f\n", pop + foo);
+  printf("result: %f\n", result);
 }
