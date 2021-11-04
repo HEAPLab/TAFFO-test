78cab615547472e34545e9952dfb28f6aad97ea4
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2021-11-04 02:16:15 +0100

    Add a run action to polybench ILP

--- polybench-c-4.2.1-beta-ilp/build.sh
+++ polybench-c-4.2.1-beta-ilp/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPTPATH=$(dirname "$BASH_SOURCE")
+export SCRIPTPATH=$(cd $(dirname "$BASH_SOURCE") && pwd)
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
 
 build_one()
@@ -30,12 +30,29 @@ build_one()
   return $err
 }
 
+run_one()
+{
+  executable="${bench}_${costmodel}_${enobweight}_${timeweight}_${castweight}"
+  float_executable="${bench}_unmodified"
+  
+  pushd "$bench" > /dev/null
+  ./$executable > taffo_out.txt 2> taffo_time.txt
+  ./$float_executable > float_out.txt 2> float_time.txt
+  
+  nfo=$($SCRIPTPATH/error.py float_out.txt taffo_out.txt)
+  err=$?
+  printf '%-19s' "$nfo"
+  
+  popd > /dev/null
+  return $err
+}
+
 clean_one()
 {
   # $1: bench name
   bench="$1"
   pushd "$bench" > /dev/null
-  rm -f *.ll *.log ${bench}_* stats.txt
+  rm -f *.ll *.log ${bench}_* *.txt
   popd > /dev/null
 }
 
@@ -52,6 +69,7 @@ if [[ -z $enobweight ]]; then export enobweight=1; fi
 if [[ -z $timeweight ]]; then export timeweight=1; fi
 if [[ -z $castweight ]]; then export castweight=1; fi
 if [[ -z $pb_dataset ]]; then export pb_dataset=SMALL_DATASET; fi
+if [[ -z $action ]];     then export action=build; fi
 
 printf 'Configuration:\n'
 printf '  costmodel  = %s\n' "$costmodel"
@@ -62,11 +80,16 @@ printf '  castweight = %s\n' "$castweight"
 printf '  pb_dataset = %s\n' "$pb_dataset"
 printf '  CFLAGS     = %s\n' "$CFLAGS"
 
-action=build
 if [[ ( $# -gt 0 ) && ( $1 == clean ) ]]; then
   action=clean
   shift
 fi
+if [[ ( $# -gt 0 ) && ( $1 == run ) ]]; then
+  action=run
+  shift
+fi
+printf '  action     = %s\n' "$action"
+
 if [[ $# -eq 0 ]]; then
   benchs=*/
 else
@@ -76,10 +99,13 @@ fi
 cd "$SCRIPTPATH/src"
 for benchdir in $benchs; do
   bench=${benchdir%/}
-  printf '%-8s %-16s' "$action" "$bench"
+  printf '%-5s %-16s' "$action" "$bench"
   case $action in
     build)
       build_one $bench
+      ;;
+    run)
+      run_one $bench
       ;;
     clean)
       clean_one $bench
